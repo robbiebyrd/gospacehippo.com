ecf0eb368fc530a9e964bc20a2e43e2553163816
robbiebyrd <me@robbiebyrd.com>
2024-01-11 11:58:37 -0600

    Tweaks

--- src/app/home/sections/Bye.jsx
+++ src/app/home/sections/Bye.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import {getColorColor as gcc} from "@/app/components/common/Colors";
+import { getColorColor as gcc } from "@/app/components/common/Colors";
 
 import Section from "@/app/components/layout/Section";
 import SectionLayout from "@/app/components/layout/SectionLayout";
@@ -11,10 +11,10 @@ export default function Bye(props) {
     return (
         <Section
             id="Byeeeee"
-            color={{color: props.color, shade: props.shade}}
+            color={{ color: props.color, shade: props.shade }}
             gradient={props.gradient}
             grid={"main"}
-            style={{zIndex: 100}}
+            style={{ zIndex: 100 }}
         >
             <SectionLayout layout={"almost-all-2"} width={90}>
                 <h2 className="color-color-dark-dark">Thanks for exploring</h2>
@@ -27,7 +27,9 @@ export default function Bye(props) {
                     }}
                 >
                     <SectionLayout width={45}>
-                        <p className={gcc("dark", "dark")}>Our site was built using:</p>
+                        <p className={gcc("dark", "dark")}>
+                            Our site was built using:
+                        </p>
                         <HeadlineList
                             tag={true}
                             color="light"
@@ -45,21 +47,27 @@ export default function Bye(props) {
                                 "HTML",
                                 "JSON",
                                 "macOS",
-                                " Silicon",
+                                "Apple Silicon",
                             ]}
                         />
                     </SectionLayout>
                     <SectionLayout width={50}>
                         <h3 className={"no-margin-bottom"}>
                             <a
-                                className={"color-color-venere-dark brand-navigation-download"}
+                                className={
+                                    "color-color-venere-dark brand-navigation-download"
+                                }
                                 href={
                                     "https://gospacehippo.com/rsrc/space-hippo-2024-brand-navigation.pdf"
                                 }
                                 download={true}
                             >
                                 Download a copy of{" "}
-                                <b>Space Hippo&rsquo;s Brand Navigation Handbook</b>.
+                                <b>
+                                    Space Hippo&rsquo;s Brand Navigation
+                                    Handbook
+                                </b>
+                                .
                             </a>
                         </h3>
                         <p className={gcc("dark", "dark")}>
